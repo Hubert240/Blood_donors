4f02409754641b1b3c3fc7d26d1e1710f36530fc
Hubert <szpiners@gmail.com>
2024-05-20 13:15:27 +0200

    first commit

---
+++ blood_donors/medicine/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    blood_group = models.CharField(max_length=10)
+    birthday = models.DateTimeField()
+
+    def __str__(self):
+        return self.user.username
---
+++ blood_donors/medicine/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+
+]
