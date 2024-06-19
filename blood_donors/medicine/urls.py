a78e53650848bcd17e6543dfa29c03fb3de2925d
kubalysiak <43576354+kubalysiak@users.noreply.github.com>
2024-06-19 21:25:41 +0200

    urls 3 es

--- blood_donors/medicine/urls.py
+++ blood_donors/medicine/urls.py
@@ -1,23 +1,17 @@
-"""
-URL configuration for blood_donors project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
+from . import views
+from django.urls import path
 urlpatterns = [
-    path('', include('medicine.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('admin_home/', views.admin_home, name='admin_home'),
+    path('make_reservation/', views.make_reservation, name='make_reservation'),
+    path('reservation_history/', views.reservation_history, name='reservation_history'),
+    path('add_medical_test_result/', views.add_medical_test_result, name='add_medical_test_result'),
+    path('reservation_history_user/', views.reservation_history_user, name='reservation_history_user'),
+    path('admin_view_medical_tests/', views.admin_view_medical_tests, name='admin_view_medical_tests'),
+    path('ranking/', views.ranking, name='ranking'),
 ]
